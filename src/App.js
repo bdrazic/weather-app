041d17448e9c40c5fdb102c1f0b67c6f910a3426
bdrazic <44465745+bdrazic@users.noreply.github.com>
2022-02-12 20:23:40 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=de95831999edbbbdf0ad558cbd7a8450`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+      setLocation("");
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Monday",
+      "Tuesday",
+      "Wednesdy",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className={
+        typeof data.main != "undefined"
+          ? data.main.temp < 16
+            ? "app cloudy"
+            : "app"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search">
+          <input
+            value={location}
+            type="text"
+            placeholder="Enter Location"
+            onKeyPress={searchLocation}
+            onChange={(event) => setLocation(event.target.value)}
+          />
+        </div>
+        <div className="container">
+          <div className="top">
+            <p className="date">{dateBuilder(new Date())}</p>
+            <div className="location">
+              <p>Location: </p>
+              <h2>{data.name}</h2>
+              <br />
+            </div>
+            <div className="temo">
+              <p>Temperature: </p>
+              {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+            </div>
+            <div className="description">
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+            <div className="weather-icon">
+              {data.weather ? (
+                <img
+                  className="weather-img"
+                  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                  alt="weather"
+                />
+              ) : null}
+            </div>
+          </div>
+
+          {data.name != undefined && (
+            <div className="bottom">
+              <div className="feels">
+                {data.main ? (
+                  <p className="bold">{data.main.feels_like.toFixed()}°C</p>
+                ) : null}
+                <p>Feels like</p>
+              </div>
+              <div className="humidity">
+                {data.main ? (
+                  <p className="bold">{data.main.humidity}%</p>
+                ) : null}
+                <p>Humidity</p>
+              </div>
+              <div className="wind">
+                {data.wind ? (
+                  <p className="bold">{data.wind.speed} km/h</p>
+                ) : null}
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
     </div>
   );
 }
